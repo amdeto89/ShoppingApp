1cb23b9a963a7eb52958feb0979f9a33ab992c69
Amdeto89 <amietoole@gmail.com>
2015-03-16 15:42:30 -0700

    updates

---
+++ java/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+	// use enter to add list items
+	$('#text').keyup(function(event){
+		if(event.keyCode == 13) {
+			event.preventDefault();
+			$('#addItem').click();
+			$('input#text').val("");
+		};
+	});	
+    var addItem = $('#addItem');
+    addItem.click(function () {
+        var toAdd = $('input#text').val();
+        $('#shoppingList').append('<li><input type="checkbox"/>' + toAdd + '</li>');
+    });
+
+    $("input:checkbox").click(function () {
+            var $this = $(this);
+            if (this.checked) {
+                $this.parent().addClass('completed');
+            } else {
+                $this.parent().removeClass('completed');
+            }
+        });
+
+});
+
+
+
